b9cf5942b25a9cdcd4cc33ca5d0b97c796d5875e
Miika Säynevirta <mayse@iki.fi>
2017-02-07 11:02:21 +0200

    Change capitalisation in migration

--- db/migrate/20170124120945_create_join_table_idea_tag.rb
+++ db/migrate/20170124120945_create_join_table_idea_tag.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableIdeaTag < ActiveRecord::Migration
   def change
-    create_join_table :Ideas, :Tags do |t|
+    create_join_table :ideas, :tags do |t|
       # t.index [:idea_id, :tag_id]
       # t.index [:tag_id, :idea_id]
     end
