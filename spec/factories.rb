ecd20d6e7746fc8d05ffa891a26d0f9dbf58a7c8
Engelbrekt <jari.silvo@cs.helsinki.fi>
2017-02-16 11:30:30 +0200

    spec files

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+	factory :user do
+		name "Testi Tauno"
+		email "testi@blaa.fi"
+		admin "false"
+		moderator "false"
+		title "opiskelija"
+		persistent_id "9876543"
+	end
+
+end
+
+
+
