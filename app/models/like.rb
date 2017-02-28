0e686eae1a2734a192d6faecbc86541d69fb4cab
Miika Säynevirta <mayse@iki.fi>
2017-02-28 14:09:39 +0200

    Verify like model references and type

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
 	belongs_to :idea
 	belongs_to :user
+
+        validates :like_type, inclusion: { in: %w(like)}
+	validates :idea, presence: true
+	validates :user, presence: true
 end
