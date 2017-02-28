65a139715e1a8ca0a4da2561deb9277505e9dcb0
Miika Säynevirta <mayse@iki.fi>
2017-02-28 14:55:10 +0200

    Update factories and tests

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Idea, type: :model do
   it "has factory make idea with all" do
     idea = FactoryGirl.create(:idea)    
+    idea.likes << FactoryGirl.create(:like, idea_id: idea.id)
     expect(idea.likes).not_to be_empty  
     expect(idea.histories).not_to be_empty      
     expect(idea.tags).not_to be_empty
