002bde2915ef28a48b3d7a7f417130f367cf6965
Miika Säynevirta <mayse@iki.fi>
2017-03-02 13:35:26 +0200

    Add validations and tests for Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,9 @@ class Like < ApplicationRecord
 	belongs_to :idea
 	belongs_to :user
 
+	validates_uniqueness_of :user, scope: :idea
+
+
         validates :like_type, inclusion: { in: %w(like)}
 	validates :idea, presence: true
 	validates :user, presence: true
