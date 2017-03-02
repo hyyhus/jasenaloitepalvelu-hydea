6a375dccdef879888795ba51f1dd23ea4253b4b6
Heikki Hei <heikkihei.ahonen@gmail.com>
2017-03-02 13:37:36 +0200

    Make idea tests pretty

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -20,44 +20,32 @@ RSpec.describe Idea, type: :model do
   end
 
   it "has factory make idea no histories" do
-    idea = FactoryGirl.create(:idea)
-    idea.histories = []
-    expect(idea.histories).to be_empty      
+    idea = FactoryGirl.build(:idea, histories: [])    
     expect(idea).not_to be_valid
   end
 
   it "has factory make idea no text" do
-    idea = FactoryGirl.create(:idea)
-    idea.text = nil
-    expect(idea.text).to be_nil
-    expect(idea).not_to be_valid
-  end
-
-  it "has factory make idea no topic" do
-    idea = FactoryGirl.create(:idea)
-    idea.topic = nil
-    expect(idea.topic).to be_nil  
+    idea = FactoryGirl.build(:idea, text: nil)
     expect(idea).not_to be_valid
   end
 
   it "has factory make idea too long topic" do
-    idea = FactoryGirl.create(:idea)
-    idea.topic = "This topic is too long for it to be valid, and should make the idea invalid and thus this test should work, nice"
-    expect(idea.topic).not_to be_nil  
+    idea = FactoryGirl.build(:idea, topic: "This topic is too long for it to be valid, and should make the idea invalid and thus this test should work, nice")
     expect(idea).not_to be_valid
   end
 
   it "has factory make idea too short topic" do
-    idea = FactoryGirl.create(:idea)
-    idea.topic = "1"
-    expect(idea.topic).not_to be_nil  
+    idea = FactoryGirl.build(:idea, topic: "1")
     expect(idea).not_to be_valid
   end
 
     it "has factory make idea blank topic" do
-    idea = FactoryGirl.create(:idea)
-    idea.topic = "         "
-    expect(idea.topic).not_to be_nil  
+    idea = FactoryGirl.build(:idea, topic: "           ")
+    expect(idea).not_to be_valid
+  end
+
+    it "has factory make idea no topic" do
+    idea = FactoryGirl.build(:idea, topic: nil)
     expect(idea).not_to be_valid
   end
 end
