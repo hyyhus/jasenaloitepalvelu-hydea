ff9465422902913b0da6b798bcc40b39b6cc9917
Engelbrekt <jari.silvo@cs.helsinki.fi>
2017-03-07 16:01:41 +0200

    User Controller spec finish

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -47,7 +47,21 @@ RSpec.describe UsersController, :type => :controller do
 			end
 		end
 
+		describe "PUT update" do
+			it "update name" do
+				@user = FactoryGirl.create(:user)		        
+		        put :update, id: @user, user: FactoryGirl.attributes_for(:user, name: "vaihdettu")	        
+		        @user.reload	        
+		        @user.name.should eq("Testi Tauno")
+			end
+		end
 
+		describe "DELETE destroy" do
+			it "don't destroy, if not admin" do
+				user = FactoryGirl.create(:user)
+		        expect{delete :destroy, id: user}.to_not change(User, :count)
+			end
+		end
 	end
 
 
@@ -73,9 +87,6 @@ RSpec.describe UsersController, :type => :controller do
 			end
 		end
 
-
-
-
 		describe "PUT update" do
 			it "update name" do
 				@user = FactoryGirl.create(:user)		        
@@ -84,7 +95,87 @@ RSpec.describe UsersController, :type => :controller do
 		        @user.name.should eq("vaihdettu")
 			end
 		end
+
+		describe "POST #create" do
+			it "create new if admin" do
+				expect{
+				post :create, user: FactoryGirl.attributes_for(:user)
+				}.to change(User, :count).by(1)				
+			end
+		end
+
+		describe "DELETE destroy" do
+			it "destroy user" do
+				user = FactoryGirl.create(:user)
+		        expect{delete :destroy, id: user}.to change(User, :count).by(-1)
+			end
+		end
+	end
+
+
+	context "Basic user logged in" do
+		before :each do
+			current_user = FactoryGirl.create(:user_student)
+		    session[:user_id] = current_user.id
+		end
+
+        #Otetaan tuotanto versiossa käyttöön
+		# describe "GET #index" do
+		# 	it "creates an array of correct users" do
+		#     user = FactoryGirl.create(:user)
+		# 	get :index
+		# 	response.should redirect_to ideas_path
+		# 	end
+		# end
+
+
+		describe "GET #show" do
+			it "redirect to ideas path, if not admin" do
+		    user = FactoryGirl.create(:user)	    
+			get :show, id: user
+			response.should redirect_to ideas_path
+			end			
+		end
+
+
+		describe "GET #new" do
+			it "doesn't get new, if not admin" do
+				get :new
+				response.should redirect_to ideas_path
+			end
+		end
+
+		describe "GET #edit" do
+			it "doesn't edit, if not admin" do
+				user = FactoryGirl.create(:user)
+				get :edit, id: user
+				response.should redirect_to ideas_path
+			end
+		end
+
+		describe "POST #create" do
+			it "doesn't create new, if not admin" do
+				expect{
+				post :create, user: FactoryGirl.attributes_for(:user)
+				}.to_not change(User, :count)
+				response.should redirect_to ideas_path
+			end
+		end
+
+		describe "PUT update" do
+			it "update name" do
+				@user = FactoryGirl.create(:user)		        
+		        put :update, id: @user, user: FactoryGirl.attributes_for(:user, name: "vaihdettu")	        
+		        @user.reload	        
+		        @user.name.should eq("Testi Tauno")
+			end
+		end
+
+		describe "DELETE destroy" do
+			it "don't destroy, if not admin" do
+				user = FactoryGirl.create(:user)
+		        expect{delete :destroy, id: user}.to_not change(User, :count)
+			end
+		end
 	end
 end
-
-
