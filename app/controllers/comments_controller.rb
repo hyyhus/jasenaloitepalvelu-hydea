2ef29adc562bd2fadd9fc30a5055f0e031184656
Miika Säynevirta <mayse@iki.fi>
2017-03-09 11:16:14 +0200

    Controller checks for moderator before deleting comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_that_is_moderator, only: [:destroy]
 
   # GET /comments
   # GET /comments.json
