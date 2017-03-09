62a8bd6f137ecfd81e7a9d2d4e9ce1010e7e28b8
Engelbrekt <jari.silvo@cs.helsinki.fi>
2017-03-09 16:45:38 +0200

    Show check_box_tag correctly for idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -74,6 +74,7 @@ class IdeasController < ApplicationController
   # PATCH/PUT /ideas/1
   # PATCH/PUT /ideas/1.json
   def update
+    byebug
     respond_to do |format|
         if @idea.update(idea_params)
         format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
