5b759956d534619e33a26e6537330ba1a2dce151
Heikki Hei <heikkihei.ahonen@gmail.com>
2017-03-09 14:30:31 +0200

    Idea controller tests

--- spec/controllers/idea_controller_spec.rb
+++ spec/controllers/idea_controller_spec.rb
@@ -1,24 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe IdeasController, :type => :controller do
-    let(:user){ FactoryGirl.create(:user) }
-    let(:user_admin){ FactoryGirl.create(:user_admin) }
-    let(:user_moderator){ FactoryGirl.create(:user_moderator) }
+RSpec.describe IdeasController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:user_admin) { FactoryGirl.create(:user_admin) }
+  let(:user_moderator) { FactoryGirl.create(:user_moderator) }
 
-    it "can be published by moderator" do
-	    session[:user_id] = user_moderator.id
-	    idea = FactoryGirl.create(:idea)
-	    expect{post :publish, id: idea.id}.to change(History,:count).by(1)
-    # Correct way to use publish is missing
-    # expect(idea.histories.last.basket).to eq("Approved")
-  end
-
-#TODO
-  it "cannot be published by non-moderator" do
+  it 'can be published by moderator' do
+    session[:user_id] = user_moderator.id
     idea = FactoryGirl.create(:idea)
+    expect { post :publish, id: idea.id }.to change(idea.histories, :count).by(1)
+    expect(response).to redirect_to ideas_path
+    expect(flash[:notice]).to eq('Idea was successfully published.')
   end
 
-  it "cannot be updated if not signed it or whatnot wrong test" do
+  # TODO
+  it 'cannot be published by non-moderator' do
+    session[:user_id] = user.id
+    idea = FactoryGirl.create(:idea)
+    expect { post :publish, id: idea.id }.not_to change(idea.histories, :count)
+    expect(response).to redirect_to ideas_path
+    expect(flash[:notice]).not_to eq('Idea was successfully published.')
+  end
+
+  it 'cannot be updated if not signed it or whatnot wrong test' do
     idea = FactoryGirl.create(:idea)
   end
 end
