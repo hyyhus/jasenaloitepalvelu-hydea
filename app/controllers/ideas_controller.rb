e5fcab421fddb0d66d20e03dba964448d42de2bb
Mikko Karvonen <mkarvonen34@gmail.com>
2017-03-10 12:26:17 +0200

    Only moderator can edit ideas tags

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,7 +1,7 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:show, :edit, :update, :destroy, :publish, :reject, :changing, :changed, :not_changed]
   before_action :ensure_that_signed_in, except: [:index, :show]
-
+  before_action :ensure_that_is_moderator, except: [:index, :show, :new]
 #  before_action :set_idea, only: [:publish]
 
   # GET /ideas
