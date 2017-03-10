399aa9c5e7049ed6b4ca443b1bed12c21e116045
Engelbrekt <jari.silvo@cs.helsinki.fi>
2017-03-10 15:08:46 +0200

    Tag update doesn't work in this version

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,49 +1,37 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:show, :edit, :update, :destroy, :publish, :reject, :changing, :changed, :not_changed]
   before_action :ensure_that_signed_in, except: [:index, :show]
-  before_action :ensure_that_is_moderator, except: [:index, :show, :new]
+  before_action :ensure_that_is_moderator, except: [:index, :show, :new, :create]
 #  before_action :set_idea, only: [:publish]
+
 
   # GET /ideas
   # GET /ideas.json
   def index
-	  if params[:basket]
-	  #basket = Basket.find_by(name: params[:basket].to_s).id
-	  #@ideas = Idea.all.where(basket: basket)
-    #@ideas = Idea.all
-    @allideas = Idea.all
-    baskets = @allideas.each { |allideas| allideas.histories.last.basket }
-    @ideas = baskets.where(basket: basket)
-
-	  else
-    @ideas = Idea.all
-	  end
+    if params[:basket]    
+      @ideas = Idea.all.select{|i| i.basket == params[:basket].to_s}
+    else
+    redirect_to '/ideas?basket=Approved'
+    end
   end
 
   # GET /ideas/1
   # GET /ideas/1.json
-  def show
-
-  end
+  def show; end
 
   # GET /ideas/new
   def new
-    if not current_user.nil?
     @idea = Idea.new
-    else
-      redirect_to ideas_path
-    end
   end
 
   # GET /ideas/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /ideas
   # POST /ideas.json
   def create
     @history = History.new
-    @history.basket = "New"
+    @history.basket = 'New'
     @history.time = Time.now
     @history.user = current_user
     @idea = Idea.new(idea_params)
@@ -61,7 +49,7 @@ class IdeasController < ApplicationController
     end
 
       respond_to do |format|
-	     if @history.save && @idea.save
+       if @history.save && @idea.save
           format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
           format.json { render :show, status: :created, location: @idea }
         else
@@ -74,9 +62,10 @@ class IdeasController < ApplicationController
   # PATCH/PUT /ideas/1
   # PATCH/PUT /ideas/1.json
   def update
+
     if params[:idea].nil?
-      @idea.tags.delete_all
-    elsif params[:idea][:tags]
+      @idea.tags.delete_all      
+    elsif params[:idea][:tags]      
       @idea.tags.delete_all
       params[:idea][:tags].each do |tag|
         newTag = Tag.find_by text: tag
@@ -84,10 +73,15 @@ class IdeasController < ApplicationController
       end
     end
 
-      respond_to do |format|
+    respond_to do |format|
+      if @idea.update(idea_params)
         format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
         format.json { render :show, status: :ok, location: @idea }
+      else
+        format.html { render :edit }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
       end
+    end    
   end
 
   # DELETE /ideas/1
@@ -101,81 +95,47 @@ class IdeasController < ApplicationController
   end
 
   def publish
-
-	  if current_user.moderator?
-	  history = History.new
-	  history.time=Time.now
-	  history.basket="Approved"
-	  history.user=current_user
-	  history.idea=@idea
-	  history.save
-
-	  end
-	  redirect_to ideas_path
-
+    if current_user.moderator?
+      @idea.histories << History.create(time: Time.now, basket: 'Approved', user: current_user, idea: @idea)
+    end
+    redirect_to ideas_path
   end
 
   def reject
-
-	  if current_user.moderator?
-	  history = History.new
-	  history.time=Time.now
-	  history.basket="Rejected"
-	  history.user=current_user
-	  history.idea=@idea
-	  history.save
-	  end
-	  redirect_to ideas_path
-
+    if current_user.moderator?
+      @idea.histories << History.create(time: Time.now, basket: 'Rejected', user: current_user, idea: @idea)
+    end
+    redirect_to ideas_path
   end
 
   def changing
-
-	  if current_user.moderator?
-	  history = History.new
-	  history.time=Time.now
-	  history.basket="Changing"
-	  history.user=current_user
-	  history.idea=@idea
-	  history.save
-	  end
-	  redirect_to ideas_path
-
+    if current_user.moderator?
+      @idea.histories << History.create(time: Time.now, basket: 'Changing', user: current_user, idea: @idea)
+    end
+    redirect_to ideas_path
   end
 
   def changed
-
-	  if current_user.moderator?
-	  history = History.new
-	  history.time=Time.now
-	  history.basket="Changed"
-	  history.user=current_user
-	  history.idea=@idea
-	  history.save
-	  end
-	  redirect_to ideas_path
-
+    if current_user.moderator?
+      @idea.histories << History.create(time: Time.now, basket: 'Changed', user: current_user, idea: @idea)
+    end
+    redirect_to ideas_path
   end
 
   def not_changed
-
-	  if current_user.moderator?
-	  history = History.new
-	  history.time=Time.now
-	  history.basket="Not Changed"
-	  history.user=current_user
-	  history.idea=@idea
-	  history.save
-	  end
-	  redirect_to ideas_path
-
+    if current_user.moderator?
+      @idea.histories << History.create(time: Time.now, basket: 'Not Changed', user: current_user, idea: @idea)
+    end
+    redirect_to ideas_path
   end
 
+
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_idea
+
+   def set_idea
       @idea = Idea.find(params[:id])
-    end
+   end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def idea_params
