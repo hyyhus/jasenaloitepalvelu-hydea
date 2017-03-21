06edc43d04e290c6c34c325b034c62b782b3f5e2
Heikki Hei <heikkihei.ahonen@gmail.com>
2017-03-21 11:48:22 +0200

    Add visible boolean to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321083431) do
+ActiveRecord::Schema.define(version: 20170321094530) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170321083431) do
     t.string   "idea_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "visible"
   end
 
   create_table "histories", force: :cascade do |t|
