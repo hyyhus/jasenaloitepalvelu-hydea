4431f7cea3fb430ceb8aeec91027ec70de7c70ef
Miika Säynevirta <mayse@iki.fi>
2017-03-23 14:38:16 +0200

    Check for comment visibility presence

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
 	belongs_to :idea
 	belongs_to :user
 
+	validates :visible, inclusion: { in: [ true, false ] }
 	validates :text,:time,:idea,:user, presence: true
 	validates_associated :idea, :user
 end
