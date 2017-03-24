04744d512bfe96caf88f73cae21c605c1b91099f
Miika Säynevirta <mayse@iki.fi>
2017-03-24 14:24:53 +0200

    Revert "Remove create session from controller"
    This reverts commit ce53a69a7fc149f4bcab65812d47f7f36f6eaf2e.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,15 @@
 class SessionsController < ApplicationController
 	def new
 	end
-	
+
+	def create
+		user = User.find_by persistent_id: params[:persistent_id]
+		# talletetaan sessioon kirjautuneen käyttäjän id (jos käyttäjä on olemassa)
+		session[:user_id] = user.id if not user.nil?
+		# uudelleen ohjataan käyttäjä omalle sivulleen
+		redirect_to user
+	end
+
 	def destroy
 		# nollataan sessio
 		session[:user_id] = nil
