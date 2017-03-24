57b65b1c87313875788456c6f6ce85f855f89ce4
Miika Säynevirta <mayse@iki.fi>
2017-03-24 13:23:13 +0200

    User tests works again

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 RSpec.describe 'UserFeature', type: :feature do
   let!(:idea){ FactoryGirl.create(:idea) }
   let!(:user){ FactoryGirl.create(:user) }
+  let!(:user_student){ FactoryGirl.create(:user_student) }
   let!(:user_moderator){ FactoryGirl.create(:user_moderator) }
   let!(:user_admin){ FactoryGirl.create(:user_admin) }
 
@@ -16,7 +17,7 @@ RSpec.describe 'UserFeature', type: :feature do
       end
 
       it 'it is not shown to other users' do
-        page.set_rack_session(:user_id => 2)
+        page.set_rack_session(:user_id => user_student.id)
         visit '/users/1'
         expect(page).not_to have_content('idea topic')
       end
