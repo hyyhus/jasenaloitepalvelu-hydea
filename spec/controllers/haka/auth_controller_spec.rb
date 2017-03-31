30f791a038964d79e013aa5e4582a06182507fb8
Engelbrekt <jari.silvo@cs.helsinki.fi>
2017-03-31 14:10:02 +0300

    remove false declaration from spec

--- spec/controllers/haka/auth_controller_spec.rb
+++ spec/controllers/haka/auth_controller_spec.rb
@@ -8,9 +8,6 @@ module Haka
 				get :new
 				expect(response).to be_redirect
 				haka_login_address = Regexp.new(Regexp.escape(Hydea::Haka::SAML_IDP_SSO_TARGET_URL))
-				Hydea::Haka::HAKA_TESTSERVER_METADATA_URL = "https://haka.funet.fi/metadata/haka-metadata.xml"
-
-
 				expect(response).to redirect_to(haka_login_address)
 		    end
 
