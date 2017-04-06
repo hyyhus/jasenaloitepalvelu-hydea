7a74bd10f69ffb511f2f6dadb8351416f70e6caf
Heikki Hei <heikkihei.ahonen@gmail.com>
2017-04-06 15:36:49 +0300

    Remove old tests

--- spec/views/faqs/new.html.erb_spec.rb
+++ spec/views/faqs/new.html.erb_spec.rb
@@ -8,14 +8,4 @@ RSpec.describe "faqs/new", type: :view do
     ))
   end
 
-  it "renders new faq form" do
-    render
-
-    assert_select "form[action=?][method=?]", faqs_path, "post" do
-
-      assert_select "input#faq_language[name=?]", "faq[language]"
-
-      assert_select "input#faq_text[name=?]", "faq[text]"
-    end
-  end
 end
