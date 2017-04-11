f560755df59437fc7c3e92f3fb2b1922d285f93e
Miika Säynevirta <mayse@iki.fi>
2017-04-11 12:54:13 +0300

    Test ban against comments and adding ideas

--- spec/features/banned_spec.rb
+++ spec/features/banned_spec.rb
@@ -10,13 +10,28 @@ RSpec.feature "Banned user", type: :feature do
     page.visit ideas_path
   end
 
+<<<<<<< Updated upstream
   it "can not add comments"
+=======
+    it "can not add comments" do
+      page.fill_in 'comment', :with => 'This will be nice'
+      click_on('Send')
+      expect(page).not_to have_content('This will be nice')
+    end
+>>>>>>> Stashed changes
 
   it "can not like ideas" do
     click_on('0')
     expect(page).not_to have_selector(:link_or_button, '1')
   end
 
+<<<<<<< Updated upstream
   it "can not add ideas"
+=======
+    it "can not add ideas" do
+      click_on('Lisää idea')
+      expect(current_path).not_to be('/ideas/new')
+    end
+>>>>>>> Stashed changes
 
 end
