f323429b48708fb7b90d797282b3b8966872c9f8
Mikko Karvonen <mkarvonen34@gmail.com>
2017-04-12 10:51:23 +0300

    Add feature spec

--- spec/features/banned_spec.rb
+++ spec/features/banned_spec.rb
@@ -1,22 +1,61 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.feature "Banned user", type: :feature do
+RSpec.feature "Banned", type: :feature do
     let!(:user) { FactoryGirl.create(:user_banned) }
     let!(:ideaone) { FactoryGirl.create(:idea_approved) }
+    let!(:user_admin){ FactoryGirl.create(:user_admin) }
 
-    before :each do
-      page.set_rack_session(user_id: user.id)
-      page.visit ideas_path
+    describe 'user' do
+      before :each do
+        page.set_rack_session(user_id: user.id)
+        page.visit ideas_path
+      end
+
+      it "can not add comments"
+
+      it "can not like ideas" do
+  			click_on('0')
+  			expect(page).not_to have_selector(:link_or_button, '1')
+      end
+
+      it "can not add ideas"
     end
 
-    it "can not add comments"
+    context 'checkbox on' do
+      describe 'banned user' do
+        before :each do
+          page.set_rack_session(user_id: user_admin.id)
+          page.visit edit_user_path(user)
+        end
+        it 'is checked' do
+          expect(find('input[name="user[banned]"]')).to be_checked
+        end
+        it 'is unchecked if user is updated' do
+          expect(find('input[name="user[banned]"]')).to be_checked
+          find('input[name="user[banned]"]').set(false)
+          click_button('Update User')
+          page.visit edit_user_path(user)
+          expect(find('input[name="user[banned]"]')).not_to be_checked
+        end
+      end
 
-    it "can not like ideas" do
-			click_on('0')
-			expect(page).not_to have_selector(:link_or_button, '1')
+      describe 'not banned user' do
+        before :each do
+          page.set_rack_session(user_id: user_admin.id)
+          normal_user = FactoryGirl.create(:user)
+          page.visit edit_user_path(normal_user)
+        end
+        it 'is unchecked' do
+          expect(find('input[name="user[banned]"]')).not_to be_checked
+        end
+        it 'is checked if user is updated' do
+          expect(find('input[name="user[banned]"]')).not_to be_checked
+          find('input[name="user[banned]"]').set(true)
+          click_button('Update User')
+          page.visit edit_user_path(user)
+          expect(find('input[name="user[banned]"]')).to be_checked
+        end
+      end
     end
-
-    it "can not add ideas"
-
 end
