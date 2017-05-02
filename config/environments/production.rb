86d32176ee7a9f6b9f72441523e74b34bfab5d7c
Heikki Hei <heikkihei.ahonen@gmail.com>
2017-05-02 14:00:00 +0300

    Create error controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # config exceptions
+  config.exceptions_app = self.routes
+  # Code is not reloaded between requxests.
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
